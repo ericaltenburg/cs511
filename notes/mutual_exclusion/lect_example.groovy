780354af413b4b244048da00b735a182328bcaa1
Eric Altenburg <ealtenbu@stevens.edu>
2020-09-09 14:37:48 -0400

    Added mutual exclusion and converted lecture 1

---
+++ notes/mutual_exclusion/lect_example.groovy
@@ -0,0 +1,24 @@
+// Turnstile example
+
+int counter = 0;
+
+def P =	Thread.start {
+	10.times {        
+		int temp = counter;
+		counter = temp +1;
+	}   
+}   
+
+
+def Q = Thread.start {
+	10.times {   
+		int temp = counter;   
+		counter = temp + 1;  // Non atmoic w/o semaphore
+	}  	
+}
+		   
+P.join(); 
+Q.join(); 
+println(counter); 
+return ;
+// Exhibit the execution path that leads to x begin 2, after the program finishes
